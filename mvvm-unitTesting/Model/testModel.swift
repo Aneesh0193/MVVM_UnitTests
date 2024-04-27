d17a7ca50dae32bd180493b800dc494bd07e610a
Aneesh <>
2024-04-27 12:17:04 -0400

    resolved merge conflicts

---
+++ mvvm-unitTesting/Model/testModel.swift
@@ -0,0 +1,17 @@
+//
+//  testModel.swift
+//  mvvm-unitTesting
+//
+//  Created by Aneesh on 27/04/24.
+//
+
+import Foundation
+
+
+class testModel {
+    
+    
+    public func model() {
+        
+    }
+}
